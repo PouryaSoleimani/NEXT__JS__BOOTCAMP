6de5b9994601a0837cb7281969120a894039e5ce
PouryaSoleimani <pouryasoleimani93@gmail.com>
2026-02-04 21:16:22 +0330

    API__ROUTES

--- app/api/employees/route.ts
+++ app/api/employees/route.ts
@@ -14,20 +14,22 @@ export async function GET(req: NextRequest) {
 export async function POST(req: Request) {
   const body = await req.json();
   console.log("REQUEST =>  ", body);
-  
+
   const result = await prisma.user.create({
     data: { email: body.email, name: body.name, password: body.password },
   });
 
+  console.log("RESULT OF POST REQUEST => ", result);
+  
   if (!result) {
-    return NextResponse.json(
-      { message: "INTERNAL SERVER ERROR ..." },
-      { status: 500 },
-    );
+      return NextResponse.json(
+          { message: "INTERNAL SERVER ERROR ..." },
+          { status: 500 },
+      );
   } else {
-    return NextResponse.json({
-      message: "USER CREATED SUCCESSFULLY ",
-      user: { name: body.name, email: body.email, password: body.password },
-    });
+      return NextResponse.json({
+          message: "USER CREATED SUCCESSFULLY ",
+          user: { name: body.name, email: body.email, password: body.password },
+      });
   }
 }
