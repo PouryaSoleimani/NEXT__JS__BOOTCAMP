e97784eddf6766471c70ba354e9cc9f058de3b2b
PouryaSoleimani <pouryasoleimani93@gmail.com>
2026-02-04 22:55:09 +0330

    API__ROUTES

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import SingleUserComponent from "@/components/SingleUserComponent";
 import prisma from "@/lib/prisma";
+import { UserType } from "@/types/dto.type";
 
 const Home = async () => {
    const users = await prisma.user.findMany();
    return (
       <section className="container min-h-dvh center-col border border-stone-700 rounded-md p-3">
          <div className="grid grid-cols-3 gap-2">
-            {users?.map((user) => (
+            {users?.map((user: UserType) => (
                <SingleUserComponent
                   key={user.id}
                   name={user.name}
