4f088cbf78263bee9677883b4e8abf512d84498c
PouryaSoleimani <pouryasoleimani93@gmail.com>
2026-02-20 12:05:51 +0330

    ADVANCED___CSS

--- components/FooterComponent.tsx
+++ components/FooterComponent.tsx
@@ -1,7 +1,75 @@
+import MingcuteFacebookFill from "@/public/icons/MingcuteFacebookFill";
+import MingcuteInstagramLine from "@/public/icons/MingcuteInstagramLine";
+import MingcuteLinkedinFill from "@/public/icons/MingcuteLinkedinFill";
+import MingcuteYoutubeFill from "@/public/icons/MingcuteYoutubeFill";
 import React from "react";
 
 const FooterComponent = () => {
-   return <footer className="footer-wrapper"></footer>;
+   return (
+      <footer className="footer-wrapper">
+         <div className="upper-footer">
+            <div className="logo_moto">
+               <h2>LOGISTICS</h2>
+               <p>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  In, voluptas?
+               </p>
+            </div>
+            <div className="footer_nav">
+               <div className="footer_nav_group">
+                  <h4>TITLE</h4>
+                  <ul>
+                     <li>LOREM</li>
+                     <li>LOREM</li>
+                     <li>LOREM</li>
+                     <li>LOREM</li>
+                     <li>LOREM</li>
+                  </ul>
+               </div>
+               <div className="footer_nav_group">
+                  <h4>TITLE</h4>
+                  <ul>
+                     <li>LOREM</li>
+                     <li>LOREM</li>
+                     <li>LOREM</li>
+                     <li>LOREM</li>
+                     <li>LOREM</li>
+                  </ul>
+               </div>
+               <div className="footer_nav_group">
+                  <h4>TITLE</h4>
+                  <ul>
+                     <li>LOREM</li>
+                     <li>LOREM</li>
+                     <li>LOREM</li>
+                     <li>LOREM</li>
+                     <li>LOREM</li>
+                  </ul>
+               </div>
+               <div className="footer_nav_group">
+                  <h4>TITLE</h4>
+                  <ul>
+                     <li>LOREM</li>
+                     <li>LOREM</li>
+                     <li>LOREM</li>
+                     <li>LOREM</li>
+                     <li>LOREM</li>
+                  </ul>
+               </div>
+            </div>
+         </div>
+
+         <div className="bottom-footer">
+            <div className="socials">
+               <MingcuteFacebookFill />
+               <MingcuteLinkedinFill />
+               <MingcuteYoutubeFill />
+               <MingcuteInstagramLine />
+            </div>
+            <div className="copyright">&copy; All Rights Reserved</div>
+         </div>
+      </footer>
+   );
 };
 
 export default FooterComponent;
